cab544ff9196d9962f35b4c86e6c0da9e32d09af
Sophea Mak <sophea_mak@manulife.com>
2023-04-25 17:48:07 +0700

    feat: init commit

---
+++ src/main/java/com/sma/webclientfeign/WebClientFeignApplication.java
@@ -0,0 +1,15 @@
+package com.sma.webclientfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactivefeign.spring.config.EnableReactiveFeignClients;
+
+@SpringBootApplication
+@EnableReactiveFeignClients
+public class WebClientFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientFeignApplication.class, args);
+    }
+
+}
